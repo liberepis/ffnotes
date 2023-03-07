f54cd32e4a5b3bb1557ff7c44f87117cdc87bfe7
Fabio Fantini <auxnet@tiscali.it>
2023-03-07 06:25:55 +0100

    x

---
+++ ps004-exer-001.ps1
@@ -0,0 +1,6 @@
+Set-StrictMode -Version 3.0
+
+Write-Host "This program read some numbers from input and make some statistical calculation"
+
+$quantity = Read-Host -Prompt "How many numbers do you want to input? "
+
