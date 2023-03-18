90ca135f0c07a6b1a60381efcc3df17edcab9f4b
Fabio Fantini <auxnet@tiscali.it>
2023-03-18 10:59:09 +0100

    First exercise

--- ps004-exer-001.ps1
+++ ps004-exer-001.ps1
@@ -4,3 +4,22 @@ Write-Host "This program read some numbers from input and make some statistical 
 
 $quantity = Read-Host -Prompt "How many numbers do you want to input? "
 
+$numbers = New-Object -TypeName System.Collections.ArrayList
+
+for ($i = 0; $i -lt $quantity; $i++){
+    $number = Read-Host -Prompt "$i Insert a number: "
+    $numbers.Add($number) | Out-Null
+}
+
+Write-Host "Statistics"
+
+$sum = 0
+
+foreach ($number in $numbers){
+    $sum += $number
+}
+
+Write-Host "The sum is $sum"
+
+$mean = $sum/$numbers.Count
+Write-Host "The mean is $mean"
