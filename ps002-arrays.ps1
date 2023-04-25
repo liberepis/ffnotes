7c7d331d9c30d0a622e9a4a7b82ed4addfcfd348
Fabio Fantini <auxnet@tiscali.it>
2023-04-25 18:44:47 +0200

    Update

--- ps002-arrays.ps1
+++ ps002-arrays.ps1
@@ -1,25 +1,53 @@
-#Arrays
+<#
+.SYNOPSIS
+Arrays in this file there are examples of use of arrays
+#>
 
+function printarray {
+    param ([System.Object[]]$vett) # param vett must be an array
+
+    $i = 0
+    foreach ($e in $vett) {
+        Write-Host "$i : $($vett[$i])"
+        $i++
+    }
+}
+
+#basic array definition
 $a = 1,2,3
-$a
+printarray -vett $a
 $a.Gettype()
-$a.count
+$a.Count
 
+#Array subexpression operator @( )
+#https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_operators?view=powershell-7.3#array-subexpression-operator--
 $b = @(1,2,3)
-$b
+printarray -vett $b
 $b.gettype()
-$b.count
+$b.Count
 
+#creating an array with only one element
 $c=,7
-$c.count
+$c.Count
 $c[0]
+printarray -vett $c
 
 #adding elements to array
+$d = $a + 4 #this create an array with one more element
+$d
 
+#this add two elements to array $a
+$d = $a + 4 + 5
+$d
+
+#this create an array with one more element and assign it to the $a variable; it is different from adding an element to an array
 $a = $a + 4
 $a
 
-#typed arrays
+#store an array in an element
+$a[3] = 10,11,12
+printarray -vett $a
 
+#typed arrays
 [Int32[]]$ia = 123, 456, 789
 $ia
