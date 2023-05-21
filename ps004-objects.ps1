4620ca99cf08918718d283ae568fb39f83eed5e8
Fabio Fantini <auxnet@tiscali.it>
2023-05-21 08:22:38 +0200

    Minor

--- ps004-objects.ps1
+++ ps004-objects.ps1
@@ -5,24 +5,6 @@ Notes on object creation and manipulating
 .DESCRIPTION
 In this file we talk about object creation and manipulating
 #>
-<<<<<<< HEAD
-Set-StrictMode -Version 3.0
-$ErrorActionPreference = Stop
-
-# Create a pscustomobject for a contact and then add properties. This is not a class, but a single instance in memory
-$firstcontact = New-Object -TypeName pscustomobject
-Add-Member -InputObject $firstcontact -MemberType NoteProperty -Name Cognome -Value ""
-Add-Member -InputObject $firstcontact -MemberType NoteProperty -Name Nome -Value ""
-Add-Member -InputObject $firstcontact -MemberType NoteProperty -Name Phone -Value ""
-
-Write-Host "Created a pscustomobject"
-$firstcontact | Get-Member
-
-# Write values in properties
-
-
-# Create an arraylist of contacts
-=======
 
 # Creating an object for a contact
 # this is not a class, is a single instance in memory
@@ -76,4 +58,3 @@ $contact.Nome = "Rossi"
 $contact.Telefono = "0612345678"
 
 $contact
->>>>>>> 5b6a3ebdb1434d3df34ae66aa313b478d66687e6
